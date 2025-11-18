673d891ee8cc2f3c9a4c8ddff77a88c979bb40df
Michael <101953642+mxayxana@users.noreply.github.com>
2025-11-18 13:52:10 -0600

    Added hamburger menu to back arrow transition effect.

--- App.js
+++ App.js
@@ -4,18 +4,29 @@ const navMenu = document.getElementById('nav-menu');
 
 const menuItems = navMenu.querySelectorAll('li');
 
+const body = document.body;
+
 function toggleMenu() {
+    document.body.classList.toggle('menu-open');
+
+    const isMenuOpen = document.body.classList.contains('menu-open');
+
     navMenu.classList.toggle('menu-open');
-
+    
     menuItems.forEach((item, index) => {
-        if (navMenu.classList.contains('menu-open')) {
+        if (isMenuOpen) {
             const delay = index * 0.1;
-
             item.style.transitionDelay = `${delay}s`;
         } else {
-            item.style.transitionDelay = '0s';
+            const reverseIndex = menuItems.length - 1 - index;
+            const delay = reverseIndex * 0.1;
+
+            item.style.transitionDelay = `${delay}s`;
         }
-    });
+    })
 }
 
-hamburgerIcon.addEventListener('click', toggleMenu);
+const menuLink = document.querySelector('.menu-link');
+menuLink.addEventListener('click', toggleMenu);
+
+// dark mode logic to be added
